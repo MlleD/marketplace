46a0139886d24690ad5c85e98a67751bbef87da0
Paul <paultsk.pt@gmail.com>
2021-02-01 13:11:56 +0100

    update hello page

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -14,10 +14,11 @@ class Routes(users: Users) extends LazyLogging {
 
     def getHello() = {
         logger.info("I got a request to greet.")
-        HttpEntity(
+        html.hello()
+        /*HttpEntity(
             ContentTypes.`text/html(UTF-8)`,
             "<h1>Say hello to akka-http</h1>"
-        )
+        )*/
     }
 
     def getSignup() = {
