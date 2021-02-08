884dacf40f0e93d6d71c93c7b49668cc4e0aa160
CADIOU leo-paul <cadiou@informatique.univ-paris-diderot.fr>
2021-02-08 12:45:43 +0100

    Cadiou test ci bd

--- src/main/scala/AppHttpServer.scala
+++ src/main/scala/AppHttpServer.scala
@@ -10,6 +10,8 @@ import com.typesafe.scalalogging.LazyLogging
 import ch.qos.logback.classic.{Level, Logger}
 import org.slf4j.LoggerFactory
 import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+import slick.jdbc.PostgresProfile.api._
+import org.postgresql.util.PSQLException
 
 
 object AppHttpServer extends LazyLogging {
@@ -41,6 +43,7 @@ object AppHttpServer extends LazyLogging {
 
         initDatabase
         val db = MyDatabase.db
+
         new RunMigrations(db)()
 
         var users = new Users()
@@ -50,12 +53,38 @@ object AppHttpServer extends LazyLogging {
         var games = new Games()
 
         /* insert data here :*/
-        developers.fillDeveloperFromCSV()
+        
+        //developers.fillDeveloperFromCSV()
+        developers.createDeveloper(0, "Leo-Paul")
+        developers.createDeveloper(0, "Paul")
+        developers.createDeveloper(0, "Laure")
+        developers.createDeveloper(0, "Dorothee")
+
+        genres.createGenre(0 ,"ActionFake")
+        genres.createGenre(0 ,"AventureFake")
+        genres.createGenre(0 ,"RPGFake")
+        genres.createGenre(0 ,"ReflexionFake")
+
+        publishers.createPublisher( 0 , "PubliFake-Leo")
+        publishers.createPublisher( 0 , "PubliFake-Paul")
+        publishers.createPublisher( 0 , "PubliFake-Laure")
+        publishers.createPublisher( 0 , "PubliFake-Dorothee")
+
+        games.createGame(0 , "Name_Fake_theWitcher3" , "basename_fake_TW3", 2 , 2005 , "Switch" , "E" , "http://www.vgchartz.com/games/boxart/full_2258645AmericaFrontccc.jpg" , 0 ,0 )
+        games.createGame(1 , "Name_Fake_theWitcher2" , "basename_fake_TW2", 2 , 2008 , "PC" , "E" , "http://www.vgchartz.com/games/boxart/full_2258645AmericaFrontccc.jpg" , 1 ,1 )
+        games.createGame(2 , "Name_Fake_Mario" , "basename_fake_Mario", 1 , 2004 , "Switch" , "E" , "http://www.vgchartz.com/games/boxart/full_2258645AmericaFrontccc.jpg" , 0 ,0 )
+        games.createGame(3 , "Name_Fake_Wii_Sport" , "basename_fake_Wii_Sport", 1 , 2000 , "PC" , "E" , "http://www.vgchartz.com/games/boxart/full_2258645AmericaFrontccc.jpg" , 1 ,1 )
+        games.createGame(4 , "Name_Fake_Age_Of_EmpireII" , "basename_fake_Age_Of_EmpireII", 3 , 2009 , "Switch" , "E" , "http://www.vgchartz.com/games/boxart/full_2258645AmericaFrontccc.jpg" , 0 ,0 )
+        games.createGame(5 , "Name_Fake_Skyrim" , "basename_fake_Skyrim", 3 , 1999 , "PC" , "E" , "http://www.vgchartz.com/games/boxart/full_2258645AmericaFrontccc.jpg" , 1 ,1 )
+
+        /*
         genres.fillGenreFromCSV()
         publishers.fillPublisherFromCSV()
         games.fillGameFromCSV()
+        */
         
-        val routes = new Routes(users, developers, genres, publishers, games)
+        
+        val routes = new Routes(users , developers , genres, publishers, games ) ; // , genres, publishers, games)
 
         val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(routes.routes)
 
--- src/main/scala/migrations/Migration09CreateTableUser.scala
+++ src/main/scala/migrations/Migration09CreateTableUser.scala
@@ -5,7 +5,7 @@ import com.typesafe.scalalogging.LazyLogging
 import slick.jdbc.PostgresProfile.api._
 
 class Migration09CreateTableUser(db: Database) extends Migration with LazyLogging {
-    class UsersTable(tag: Tag) extends Table[(String, String)](tag, "users") {
+    class UserTable(tag: Tag) extends Table[(String, String)](tag, "user") {
         def userId = column[String]("userId", O.PrimaryKey)
         def username = column[String]("username")
         def * = (userId, username)
@@ -13,10 +13,10 @@ class Migration09CreateTableUser(db: Database) extends Migration with LazyLoggin
 
     override def apply(): Unit = {
         implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
-        val dropFuture: Future[Unit] = db.run(TableQuery[UsersTable].schema.dropIfExists)
-        dropFuture.map(t => logger.info("Done dropping table Users"))
+        val dropFuture: Future[Unit] = db.run(TableQuery[UserTable].schema.dropIfExists)
+        dropFuture.map(t => logger.info("Done dropping table User"))
 
-        val creationFuture: Future[Unit] = db.run(TableQuery[UsersTable].schema.createIfNotExists)
+        val creationFuture: Future[Unit] = db.run(TableQuery[UserTable].schema.createIfNotExists)
         creationFuture.onComplete(t => logger.info("Done creating table User"))
     }
 }
