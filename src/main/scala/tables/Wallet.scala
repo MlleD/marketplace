76c85971b9465b232730307c4929797cbe8d03ad
Paul <paultsk.pt@gmail.com>
2021-03-06 21:32:50 +0100

    ajout test de routes

--- src/main/scala/tables/Wallet.scala
+++ src/main/scala/tables/Wallet.scala
@@ -4,6 +4,6 @@ import slick.jdbc.PostgresProfile.api._
 
 class WalletTable(tag: Tag) extends Table[(Int, Int)](tag, "Wallets") {
     def id = column[Int]("WalletId", O.PrimaryKey)
-    def solde = column[Int]("WalletSolde")
+    def solde = column[Int]("WalletSolde",O.Default(0))
     def * = (id, solde)
 }
